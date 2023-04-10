0eeebcf1f3d7f8ec6314e7e34b15747af6a06125
Michal Landsman <landsman@studioart.cz>
2023-04-11 01:55:34 +0200

    time slot - whole day message

--- src/components/calendar/time-slot/slot.tsx
+++ src/components/calendar/time-slot/slot.tsx
@@ -1,17 +1,44 @@
 import {EventType} from "@local/api/view/events/types/event-type";
 import styled from "styled-components";
 import {useAppContext} from "@local/runtime";
+import {differenceInHours, format, parseISO} from "date-fns";
+import {t} from "@lingui/macro";
+import {useLingui} from "@lingui/react";
 
 type PropTypes = {
     event: EventType; // todo: sent just necessary data!
 }
 
+const wholeDay = 8;
+
 export function TimeSlot({ event }: PropTypes): JSX.Element {
-    const { setSelectedEvent } = useAppContext();
+    const { setSelectedEvent, localeDataForCalendar } = useAppContext();
+    const { i18n } = useLingui();
+
+    const opts = {
+        locale: localeDataForCalendar,
+    };
+
+    // convert for date-fns
+    const fromParsed = parseISO(event.dateFrom.toString());
+    const toParsed = parseISO(event.dateTo.toString());
+
+    const eventDuration = differenceInHours(fromParsed, toParsed) * -1;
+
+    // show localised message instead of hours
+    let text: JSX.Element;
+    if (eventDuration >= wholeDay) {
+        text = <span>{i18n._(t`calendar.time_slot.whole_day`)}</span>;
+    } else {
+        const fromFormatted = format(fromParsed, 'p', opts);
+        const toFormatted = format(toParsed, 'p', opts);
+        text = <span>{fromFormatted} - {toFormatted}</span>;
+    }
+
     return (
         <Wrapper>
             <Content onClick={() => setSelectedEvent(event.id)}>
-                {event.dateFrom.toString()}
+                {text}
             </Content>
         </Wrapper>
     );
