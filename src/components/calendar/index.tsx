83473f4076eb7e04e6ea000cfcdb3e64913eeff5
Michal Landsman <landsman@studioart.cz>
2023-04-11 03:57:19 +0200

    hide footer when you change month

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from "styled-components";
 import {DayPicker} from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
@@ -15,6 +15,7 @@ type PropTypes = {
 export function Calendar({ widgetTheme }: PropTypes) {
     const { i18n } = useLingui();
     const { localeDataForCalendar, features, events, date, setDate } = useAppContext();
+    const [showFooter, setShowFooter] = useState<boolean>(true);
 
     /** show skeleton until app context fetch locale data */
     if (undefined === localeDataForCalendar) {
@@ -23,19 +24,31 @@ export function Calendar({ widgetTheme }: PropTypes) {
         );
     }
 
-    let footer = <div />;
-
-    if (features.allowTimeSlots &&
-        undefined !== events &&
-        events?.length > 0
-    ) {
-        footer = <ListOfSlots events={events} />;
-    }
+    /**
+     * place under calendar with time slots
+     */
+    const footer = () => {
+        const anyEvents = undefined !== events && events?.length > 0;
+        if (showFooter && features.allowTimeSlots && anyEvents) {
+            return <ListOfSlots events={events} />;
+        }
+        return <span />;
+    };
 
     /** set date to app context */
     const handleSelected = (day: Date | undefined, selectedDay: Date) => {
         setDate(selectedDay!);
-    }
+        setShowFooter(true);
+    };
+
+    /** hide footer when change month */
+    const handleOnMonthChange = (month: Date) => {
+        if (date?.getMonth() === month.getMonth()) {
+            setShowFooter(true);
+        } else {
+            setShowFooter(false);
+        }
+    };
 
     return (
         <Wrapper>
@@ -44,7 +57,8 @@ export function Calendar({ widgetTheme }: PropTypes) {
                 locale={localeDataForCalendar}
                 selected={date}
                 onSelect={handleSelected}
-                footer={footer}
+                onMonthChange={handleOnMonthChange}
+                footer={footer()}
                 showOutsideDays
             />
         </Wrapper>
