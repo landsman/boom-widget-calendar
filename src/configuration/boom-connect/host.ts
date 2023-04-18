d63d49dff524956633d307eec7c7a38828771e37
Michal Landsman <landsman@studioart.cz>
2023-04-18 15:59:33 +0200

    revert host to stage again

--- src/configuration/boom-connect/host.ts
+++ src/configuration/boom-connect/host.ts
@@ -1,5 +1,5 @@
 export function connectHost(production: boolean): string {
     return production
         ? 'https://connect.boomevents.org'
-        : 'https://pr-692---app-connect-nwh4rg47xq-ew.a.run.app';
+        : 'https://connect.boomevents.dev';
 }
