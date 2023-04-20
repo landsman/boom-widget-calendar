1af21a833d08ceb4647f0ce5cb3f2356e07f0d5d
Michal Landsman <landsman@studioart.cz>
2023-04-20 16:11:26 +0200

    not bad

--- public/api/loader.js
+++ public/api/loader.js
@@ -1,5 +1,72 @@
 alert("yep its working");
-console.log("_boomEventsCalendar", _boomEventsCalendar);
+
+/**
+ * api object to be used bellow
+ */
+const api = window?.BOOM_WIDGET_API_CALENDAR;
+const snippetId = 'boom-events-org-calendar';
+const host = '//localhost:3000/boom-widget-calendar/';
+
+/**
+ * util
+ */
+function getUnixTime() {
+    const date = new Date();
+    return Math.floor(date.getTime() / 1000);
+}
+
+/**
+ * util
+ */
+function getIdValue(what) {
+    return 'boom-events-org-calendar-loader-' + what;
+}
+
+/**
+ * install css
+ */
+function stylesInstall() {
+    // find place where is installed script
+    const install = document.getElementById(snippetId);
+
+    const element = document.createElement('link');
+    element.rel = 'stylesheet';
+    element.id = getIdValue('css');
+    element.href= host + '/api/loader.css?v=' + getUnixTime();
+
+    // insert a new external style file to load
+    install.parentNode.insertBefore(element, install);
+}
+
+/**
+ * install iframe
+ * todo: move api paras to object
+ */
+function iframeInstall() {
+    // find place where is installed script
+    const install = document.getElementById(snippetId);
+
+    const element = document.createElement('iframe');
+    element.id = getIdValue('iframe');
+    element.src = host + '?v=' + getUnixTime() + '&isProduction='+ api.isProduction + '&organizerId=' + api.organizerId;
+
+    // insert a new external style file to load
+    install.parentNode.insertBefore(element, install);
+}
+
+/**
+ * run the whole thing
+ */
+if (null !== api) {
+    console.log("BOOM_WIDGET_API_CALENDAR", api);
+    stylesInstall();
+    iframeInstall();
+} else {
+    console.error("BOOM_WIDGET_API_CALENDAR is null!");
+}
+
+
+
 
 //
 // <style type="text/css">
