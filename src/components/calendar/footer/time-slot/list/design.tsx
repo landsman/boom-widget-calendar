05a8433dc75dc25fc2d69f72f43935afd2b05b9c
Michal Landsman <landsman@studioart.cz>
2023-04-24 15:21:28 +0200

    time slots - indent fix

--- src/components/calendar/footer/time-slot/list/design.tsx
+++ src/components/calendar/footer/time-slot/list/design.tsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
 export const ListWrapper = styled.div`
-  max-width: 100%;
+  width: calc(100% + 10px);
   padding: 0;
   
   display: flex;
   flex-wrap: wrap;
   margin-top: 10px;
+  margin-left: -10px;
   
   justify-content: flex-start;
 `;
