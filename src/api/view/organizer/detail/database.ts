c163ac95b7541b888c691186044441ba1de062eb
Michal Landsman <landsman@studioart.cz>
2023-05-25 10:25:04 +0200

    pbrana - fix production theme

--- src/api/view/organizer/detail/database.ts
+++ src/api/view/organizer/detail/database.ts
@@ -48,7 +48,7 @@ export const organizersDatabase: Organizer[] = [
         // Pravčická brána @ prod
         id: "8fabf06c-0078-47e4-9c30-3f17827da0ab",
         production: true,
-        customTheme: CustomTheme.KVANTARIO,
+        customTheme: CustomTheme.PRAVCICKA_BRANA,
         features: null,
     },
     {
